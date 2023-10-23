c464f02cc65a6277aef41f79e5c80ba939011101
nikita-restdali <nikita@restdali.com>
2023-10-24 00:30:18 +0530

    adding img and changing dir with resp to lang

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom';
 import i18n from "i18next";
 import {initReactI18next } from "react-i18next";
@@ -24,15 +24,24 @@ i18n
     backend: {
       loadPath: '/assets/locales/{{lng}}/translation.json',
     },
-    react: {
-      useSuspense: false,
-    }
+
+    //useSupense when something not loaded
+    // react: {
+    //   useSuspense: false,
+    // }
     
   });
-
+const loadingMarkup = (
+  <div className='py-4 text-center'>
+    <h2>Loading...</h2>
+  </div>
+)
     ReactDOM.render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>,
-      document.getElementById('root')
+      <Suspense fallback={loadingMarkup}>
+          <React.StrictMode>
+            <App />
+        </React.StrictMode>
+      </Suspense>,
+       document.getElementById('root')
+      
     );
