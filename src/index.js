ce170cc4c712e91563f320aaa12822b64bf16c95
nikita-restdali <nikita@restdali.com>
2023-10-23 14:56:13 +0530

    by cookies can change lang

--- src/index.js
+++ src/index.js
@@ -1,7 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
 import './index.css';
-import App from './App';
+// import App from './App';
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    
+    fallbackLng: "en",
+    detection: {
+      order: ['cookie', 'htmlTag',  'localStorage','path', 'subdomain'],
+      caches:['cookie'],
+    },
+    backend: {
+      loadPath: '/assets/locales/{{lng}}/translation.json',
+    },
+    react: {
+      useSuspense: false,
+    }
+    
+  });
+
+function App() {
+  const { t } = useTranslation();
+
+  return <h2>{t('welcome')}</h2>;
+}
+
+
 
 ReactDOM.render(
   <React.StrictMode>
