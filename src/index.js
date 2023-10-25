f6dbcca9807a367f5dbdad4683828073225fa1c4
nikita-restdali <nikita@restdali.com>
2023-10-26 00:22:02 +0530

    lang select acc to path

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ i18n
     supportedLngs:['en', 'fr', 'ar'],
     fallbackLng: "en",
     detection: {
-      order: ['cookie', 'htmlTag',  'localStorage','path', 'subdomain'],
+      order: ['path','cookie', 'htmlTag',  'localStorage', 'subdomain'],
       caches:['cookie'],
     },
     backend: {
